fe51b3b965b9c70bd2dd20d534647c6a0c58f2c9
Don Ostergaard <oster012@rangers.uwp.edu>
2018-04-12 17:12:53 -0500

    base logic for the calendar is now complete

--- Tips N Stuff/TipCalendarViewController.swift
+++ Tips N Stuff/TipCalendarViewController.swift
@@ -2,14 +2,74 @@
 //  TipCalendarViewController.swift
 //  Tips N Stuff
 //
-//  Created by Aaron Diaz on 4/10/18.
-//  Copyright © 2018 Aaron Diaz. All rights reserved.
+//  Created by Don Ostergaard on 4/10/18.
+//  Copyright © 2018 Don Ostergaard. All rights reserved.
 //
 
 import UIKit
+import EventKit
+import EventKitUI
 
 class TipCalendarViewController: UIViewController {
 
+    @IBOutlet weak var jobField: UITextField!
+    @IBOutlet weak var tipAmount: UITextField!
+    @IBOutlet weak var DateField: UITextField!
+    @IBOutlet weak var NotesField: UITextField!
+    
+    
+    @IBAction func pushToCalendar(_ sender: UIButton) {
+        // All fields in the calendar need to be strings
+        let titles: String = jobField.text!
+        let descriptions: String = tipAmount.text!
+        let locations: String = "\n " + NotesField.text!
+        let DateTime = Date()                          // the date the tip is made
+        
+        let eventStore: EKEventStore = EKEventStore()
+        // ask user for permission to their calendar so we can add a project event in the calendar
+        eventStore.requestAccess(to: .event) {(granted, error) in
+            if(granted) && (error == nil)
+            {
+                print("granted\(granted)")
+                print("error \(String(describing: error))")
+                
+                let event: EKEvent = EKEvent (eventStore: eventStore)
+                // adding project fields to the actual calendar
+                event.title = titles
+                event.startDate = DateTime
+                event.endDate = DateTime
+                event.notes = descriptions + locations
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    // save the event to the ios calendar
+                    try eventStore.save(event, span: .thisEvent)
+                    // launch ios calendar app with the newly created saved project event
+                    self.gotoAppleCalendar(date: event.startDate! as NSDate)
+                    
+                    
+                }catch let error as NSError{
+                    print ("error : \(error)")
+                }
+                print ("Save Event")
+                
+            }else{
+                print("error := \(String(describing: error))")
+            }
+        }
+    }
+    
+    // This function enables the Maruf app to launch the ios calendar with new events.
+    func gotoAppleCalendar(date: NSDate) {
+        let interval = date.timeIntervalSinceReferenceDate
+        // use a system call to notify Maruf to go to the calendar app
+        let url = NSURL(string: "calshow:\(interval)")!
+        UIApplication.shared.openURL(url as URL)
+    }
+    
+    @IBOutlet weak var todayTipField: UITextField!
+    
+    @IBOutlet weak var yearsTipField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
