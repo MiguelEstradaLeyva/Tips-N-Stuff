501768d4121bf8beefa6a98efa5db06f25f0d0c7
Aaron <diaz0029@rangers.uwp.edu>
2018-04-12 15:35:39 -0500

    Initial Commit

---
+++ Tips N Stuff/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  Tips N Stuff
+//
+//  Created by Aaron Diaz on 4/10/18.
+//  Copyright © 2018 Aaron Diaz. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
